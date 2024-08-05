c505ec1a19da2fa139000bbef33005ec51fa93cd
nirmit152007 <kiitcommon@gmail.com>
2024-08-05 05:38:37 +0530

    api endpoint setup

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Basic GET endpoint
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
+// Middleware to parse the JSON bodies of POST requests
+app.use(bodyParser.json());
+
+// POST endpoint for /slack/action-endpoint
+app.post("/slack/action-endpoint", (req, res) => {
+  console.log("Received a request:", req.body);
+
+  // Respond to Slack with a 200 OK status
+  res.status(200).send("Action received");
 });
 
 // Start the server
